485981f0db308c7932ba33e111caa21b337d06fe
rluetken-dev <rluetken.dev@gmail.com>
2025-09-09 13:49:26 +0200

    docs(swagger): enable XML comments and annotations

--- Portfolio.Api/Program.cs
+++ Portfolio.Api/Program.cs
@@ -3,6 +3,9 @@ using Polly.Extensions.Http;
 using Portfolio.Api.Services;
 using Portfolio.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Swashbuckle.AspNetCore.Annotations;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,7 +34,17 @@ builder.Services.AddHttpClient<AlphaVantageClient>(client =>
 // --- rest stays the same ---
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Enable [SwaggerOperation]/[SwaggerResponse] attributes
+    c.EnableAnnotations();
+
+    // Include XML comments from this assembly
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+        c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
